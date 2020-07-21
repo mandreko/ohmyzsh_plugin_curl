f1a5825ffab54d2d63d42ce521553dca3e2a4e9b
Matt Andreko <mandreko@gmail.com>
2020-07-21 10:58:38 -0400

    First commit

---
+++ curl.plugin.zsh
@@ -0,0 +1,5 @@
+alias curl_ie="curl -A 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)'"
+alias curl_ff="curl -A 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0'"
+alias curl_c="curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'"
+
+
